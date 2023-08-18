6d19d6a41d72df63b3c263d7cdbfa2f95e101151
mustapha ibrahim <mustaphaibrahim566@gmail.com>
2023-08-18 22:59:56 +0000

    changing update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-      if len(matrix) != 0:
-        sqr = []
-        for row in matrix:
-            new = []
-            for pack in row:
-                new.append(pack**2)
-            sqr.append(new)
-        return sqr
+    squr = [[x ** 2 for i in row] for row in matrix]
+    return sqr
